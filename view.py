9899dcdc19aca7514d9ff7e026530da4cd6f2552
Renan (Pessoal) <renfbatista@gmail.com>
2020-04-16 22:46:55 -0300

    16-04

--- view.py
+++ view.py
@@ -1,2 +1,17 @@
-function file(name):
-	print(name)
+import sys
+
+
+class bcolors:
+    DIRETORIO = '\033[94m'
+    ARQUIVO = '\033[92m'
+    ENDC = '\033[0m'
+
+
+def filePrinter(nome, novoDestino, diretorioAtual, profundidade):
+    sys.stdout.write('|')
+    i = 0
+    while(i < profundidade):
+        sys.stdout.write('-')
+        i += 1
+    print(bcolors.DIRETORIO + novoDestino + "    " +
+          diretorioAtual + '/' + novoDestino + bcolors.ENDC)
